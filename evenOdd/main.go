2971bce50db3764f5e93b75dc7d917e52908333e
Priyank Rege <priyank.rege@bigcommerce.com>
2023-09-08 18:21:24 -0500

    executable packages

--- evenOdd/main.go
+++ evenOdd/main.go
@@ -1,9 +1,9 @@
-package evenOdd
+package main
 
 func main() {
-	tenNumbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for _, number := range tenNumbers {
+	for _, number := range numbers {
 		if number%2 == 0 {
 			println(number, "is even")
 		} else {
