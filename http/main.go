e58c63cdb1107f8b8ac892884b830d49ecf68399
Priyank Rege <priyank.rege@bigcommerce.com>
2023-09-10 13:19:46 -0500

    Custom interfaces

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	const URL = "https://www.google.com"
@@ -16,10 +19,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	_, err = resp.Body.Read(bs)
-	if err != nil {
+	_, err2 := io.Copy(os.Stdout, resp.Body)
+
+	if err2 != nil {
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+
+	_, err3 := io.Copy(logWriter{}, resp.Body)
+
+	if err3 != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote these many bytes:", len(bs), "bytes")
+	return len(bs), nil
 }
